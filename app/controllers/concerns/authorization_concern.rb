4f7f9abf20d477161fe7924d79c83cb456dff632
Fabricio Nascimento <fabriciosn@gmail.com>
2016-06-26 15:32:55 -0300

    Fix a comment on is_owner authorization

--- app/controllers/concerns/authorization_concern.rb
+++ app/controllers/concerns/authorization_concern.rb
@@ -10,6 +10,7 @@ module AuthorizationConcern
     return false unless user.respond_to?(:id)
 
     return false unless object
+    # Objects that do not belong to anyone, can be edit by everyone
     return true unless object.respond_to?(:user_id)
 
     return true if user.admin?
