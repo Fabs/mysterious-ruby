3d70537c3b9d94de322e52a3614d79d04bfb88e9
Fabricio Nascimento <fabriciosn@gmail.com>
2016-06-27 01:57:15 -0300

    Fix disables sprockets

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Api
   class Application < Rails::Application
+    config.assets.enabled = false
     # Use the responders controller from the responders gem
     config.app_generators.scaffold_controller :responders_controller
 
